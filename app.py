cfdb7aae694dfc46170a9d425406833cb219de41
Maciej Ledzion <maciejledzion96@gmail.com>
2021-06-27 15:50:32 +0200

    update

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def homepage():
-    movies = ['skazani na shawshank', "zielona mila"]
-    print("asxksdmcjdcj")
+    movies = ['skazani na shawshank', "zielona mila",'ironman',"jamesbond", 'forest gump', 'pulp fiction']
     return render_template("homepage.html", movies=movies)
 
 if __name__ == '__main__':
